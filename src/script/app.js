a7e6d89a7f54f0fc46382dafa7daa6310567a944
TohidAbdullazade <tohidabdullazade@gmail.com>
2024-04-30 12:04:11 +0400

    I write an simple greeting app with  Javascript

---
+++ src/script/app.js
@@ -0,0 +1,8 @@
+// ===> Variables <=== 
+var name = prompt("Enter your Name please");
+var surname = prompt("Enter your Surname please");
+var yearOfBirth = +prompt("Enter your year of birth");
+var age = 2024 - yearOfBirth;
+
+// // ===> Show Function <===
+alert(`Your name is: ${name} your surname is: ${surname} your age is: ${age}`);
